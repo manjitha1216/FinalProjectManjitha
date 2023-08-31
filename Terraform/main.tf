690bf1ba8c1124e9ad4b0c1a2c27c08c55f3d851
Manjitha Manchanayake <129063718+manjitha1216@users.noreply.github.com>
2023-08-31 13:35:23 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,78 @@
+#creating instance using terraform
 
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "Manjitha_Terraform" {
+  ami             = "ami-04e601abe3e1a910f"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.security_group_Manjitha.name]
+  key_name        = aws_key_pair.id_rsa.key_name
+  tags = {
+    "Name" = "Manjitha_EC2_Instance"
+  }
+}
+
+#Create security group using terraform
+
+resource "aws_security_group" "security_group_Manjitha" {
+  name        = "security_group_Manjitha"
+  description = "create security group using terraform"
+  vpc_id      = "vpc-02d12aefa4d6ff8b7"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "security_group_Manjitha"
+  }
+}
+
+
+#Add key-pair
+
+resource "aws_key_pair" "id_rsa" {
+  key_name   = "id_rsa"
+  public_key = file("id_rsa.pub")
+}
